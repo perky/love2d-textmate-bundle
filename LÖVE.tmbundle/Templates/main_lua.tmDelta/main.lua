68894ae8793bd3cd8dbb1c421b84dc3a1173694e
Luke Perkin <luke.perkin@gmail.com>
2010-03-21 10:33:33 +0000

    Competion now adds functions local to document.
    also includes class inheritance and comments.

---
+++ LÖVE.tmbundle/Templates/main_lua.tmDelta/main.lua
@@ -0,0 +1,40 @@
+-- Filename: ${TM_FILENAME}
+-- Author: ${TM_FULLNAME}
+-- Date: ${TM_DATE}
+
+-- Initialization
+function love.load()
+
+end
+
+-- Logic
+function love.update(dt)
+
+end
+-- Scene Drawing
+function draw()
+
+end
+
+-- Scene Drawing
+function draw()
+
+end
+
+-- Input
+function love.mousepressed(x,y,button)
+
+end
+
+function love.mousereleased(x,y,button)
+
+end
+
+function love.keypressed(key,unicode)
+
+end
+
+function love.keyreleased(key,unicode)
+
+end
+
